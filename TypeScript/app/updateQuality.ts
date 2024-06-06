0cec3085ecffeb790bb9414690c7c2d08ee9c60e
Omar Zikry <omar@omarzikry.com>
2024-06-06 22:01:16 +0300

    Extra refactoring

--- TypeScript/app/updateQuality.ts
+++ TypeScript/app/updateQuality.ts
@@ -2,31 +2,38 @@ import { Item } from "./item";
 
 const MAXIMUM_QUALITY = 50;
 const MINIMUM_QUALITY = 0;
+const SULFURAS_QUALITY = 80;
 
-const isLessThanMaximum = quality => quality < MAXIMUM_QUALITY;
-const isOverMinimum = quality => quality > MINIMUM_QUALITY;
+const isLessThanMaximum = (quality: number): boolean => quality < MAXIMUM_QUALITY;
+const isOverMinimum = (quality: number): boolean => quality > MINIMUM_QUALITY;
 
-const increaseQuality = quality => isLessThanMaximum(quality) ? quality + 1 : quality;
-const decreaseQuality = quality => isOverMinimum(quality) ? quality - 1 : quality;
+const increaseQuality = (quality: number): number => isLessThanMaximum(quality) ? quality + 1 : quality;
+const decreaseQuality = (quality: number): number => isOverMinimum(quality) ? quality - 1 : quality;
 
-const updateItemQuality = (item): Item => {
+const updateItemQuality = (item: Item): Item => {
     item.quality = decreaseQuality(item.quality);
     item.quality = item.sellIn <= 0 ? decreaseQuality(item.quality) : item.quality;
 
     return item;
 };
 
-export const updateQualityForGeneralItem = (item): Item => {
-    item = updateItemQuality(item);
+const decreaseSellIn = (item: Item): Item => {
     item.sellIn -= 1;
+    return item;
+};
+
+export const updateQualityForGeneralItem = (item: Item): Item => {
+    item = updateItemQuality(item);
+    item = decreaseSellIn(item);
 
     return item;
 };
 
-export const updateQualityForAgedBrie = (item): Item => {
+
+export const updateQualityForAgedBrie = (item: Item): Item => {
     item.quality = increaseQuality(item.quality);
     item.quality = item.sellIn < 0 ? increaseQuality(item.quality) : item.quality;
-    item.sellIn -= 1;
+    item = decreaseSellIn(item);
 
     return item;
 };
@@ -39,15 +46,15 @@ const increaseQualityForConcert = (item: Item): number => {
     return quality;
 };
 
-export const updateQualityForConcert = (item): Item => {
+export const updateQualityForConcert = (item: Item): Item => {
     item.quality = item.sellIn === 0 ? 0 : increaseQualityForConcert(item);
-    item.sellIn -= 1;
+    item = decreaseSellIn(item);
 
     return item;
 };
 
 export const updateQualityForSulfuras = (item: Item): Item => {
-    item.quality = 80;
+    item.quality = SULFURAS_QUALITY;
 
     return item;
 };
@@ -55,7 +62,7 @@ export const updateQualityForSulfuras = (item: Item): Item => {
 export const updateQualityForConjured = (item: Item): Item => {
     item = updateItemQuality(item);
     item = updateItemQuality(item);
-    item.sellIn -= 1;
+    item = decreaseSellIn(item);
 
     return item;
 };
